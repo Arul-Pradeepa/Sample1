a6f5ea3c44ca7977cbe4273a4d66bd35cba831d2
Arul-Pradeepa <arulantony1607@gmail.com>
2022-10-18 11:15:40 -0700

    done

--- src/org/github/GitHub.java
+++ src/org/github/GitHub.java
@@ -11,7 +11,8 @@ public static void main(String[] args) {
 		int i = 100000;
 		System.out.println("my salary is:1000");
 		
-		
+		System.out.println("DONE");
+		System.out.println("SUCCESS");
 	}
 	
 
